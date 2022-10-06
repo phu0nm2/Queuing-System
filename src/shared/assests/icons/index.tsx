7ca2122971dbc467bbde5c0ea4487dab2b86395a
xuannhutzz <xuannhutzz@gmail.com>
2022-10-06 10:54:57 +0700

    style: new sidebar and format code

---
+++ src/shared/assests/icons/index.tsx
@@ -0,0 +1,10 @@
+const icons = {
+  dashboardIcon: require('../../assests/icons/dashboard.svg').default,
+  desktopIcon: require('../../assests/icons/desktop.svg').default,
+  serviceIcon: require('../../assests/icons/service.svg').default,
+  numberLevelIcon: require('../../assests/icons/numberLevel.svg').default,
+  reportIcon: require('../../assests/icons/report.svg').default,
+  settingIcon: require('../../assests/icons/setting.svg').default,
+  logoutIcon: require('../../assests/icons/logout.svg').default,
+};
+export default icons;
