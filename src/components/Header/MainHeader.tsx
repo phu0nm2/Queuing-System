7a99521141f9597e0f909648a980499131e20899
Thanh <thanhnguyen96.glinking@gmail.com>
2022-11-02 22:36:19 +0700

    merge fail

--- src/components/Header/MainHeader.tsx
+++ src/components/Header/MainHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Layout } from 'antd';
-import MainBreadcrumb from '../Breadcrumb';
+import MainBreadcrumb from '../BreadCrumb';
 import Profile from '../Profile';
 import styled from 'styled-components';
 
