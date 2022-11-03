9fd481acda09d827d4bac84047c5e1d6042d2bcd
Thanh <thanhnguyen96.glinking@gmail.com>
2022-11-03 21:57:45 +0700

    delete 2 file component and pages to merge again

--- src/pages/Setting/LogUser/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LogUser = () => {
-  return <div>LogUser</div>;
-};
-
-export default LogUser;
--- src/pages/Setting/index.tsx
+++
@@ -1,5 +0,0 @@
-import RoleManagement from './RoleManagement';
-import AccountManagement from './AccountManagement';
-import LogUser from './LogUser';
-
-export { RoleManagement, AccountManagement, LogUser };
